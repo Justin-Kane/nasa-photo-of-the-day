d45a27d126eabeb73424e0d64c2cc90ab6694dce
Justin Kane <JustinKane197@gmail.com>
2019-09-12 01:09:20 -0400

    meets MVP

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from 'axios';
+
+import Title from './title';
+import Date from './date';
+import Explanation from './explanation';
+import Media from './media';
+
 
 function App() {
+
+  const [space, setSpace] = useState();
+  let title = null;
+  let date = null;
+  let explanation = null;
+  let url = null;
+
+
+  useEffect ( () => {
+    axios
+      .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+      .then(result => {
+        setSpace(result.data)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }, [] );
+
+  if(space) {
+    title = space.title;
+    date = space.date;
+    explanation = space.explanation;
+    url = space.url;
+  }
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <div className="App-header">
+        <h1>Nasa Picture of the Day</h1>
+        <Title title = {title}/>
+        <Date date = {date}/>
+        <Media url = {url}/>
+        <Explanation explanation = {explanation}/>
+      </div>
     </div>
   );
 }
---
+++ src/media.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Media = ( {url} ) => (
+    <img src={url} className="img" alt="Media" />
+);
+
+export default Media;
